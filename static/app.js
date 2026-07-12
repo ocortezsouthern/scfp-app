1e384c5b342ab63ccc9cadec1d902f8af426fa61
ocortezsouthern <ocortezsouthern@gmail.com>
2026-07-12 16:59:10 -0400

    Add files via upload

--- static/app.js
+++ static/app.js
@@ -2,7 +2,7 @@
 (function () {
   function buildInput(field, col, rowIdx, prefill) {
     var name = "tbl_" + field + "_" + rowIdx + "_" + col.key;
-    var value = prefill !== undefined ? prefill : "";
+    var value = prefill !== undefined && prefill !== null ? String(prefill) : "";
     if (col.type === "select") {
       var opts = (col.options || [""]).map(function (o) {
         var sel = o === value ? " selected" : "";
@@ -19,12 +19,21 @@
     return "<input type=\"text\" name=\"" + name + "\" value=\"" + value.replace(/"/g, "&quot;") + "\">";
   }
 
-  function addRow(container, field, columns, rowIdx, prefillFirst) {
+  // rowData: either a plain string (prefills just the first column, used for
+  // brand-new inspections seeded from default_rows) or an object keyed by
+  // column key (used to fully restore a saved row when editing).
+  function addRow(container, field, columns, rowIdx, rowData) {
     var tbody = container.querySelector("tbody");
     var tr = document.createElement("tr");
+    var isObject = rowData && typeof rowData === "object";
     columns.forEach(function (col, i) {
       var td = document.createElement("td");
-      var prefill = (i === 0 && prefillFirst) ? prefillFirst : undefined;
+      var prefill;
+      if (isObject) {
+        prefill = rowData[col.key];
+      } else if (i === 0 && rowData) {
+        prefill = rowData;
+      }
       td.innerHTML = buildInput(field, col, rowIdx, prefill);
       tr.appendChild(td);
     });
@@ -42,9 +51,14 @@
       var field = container.getAttribute("data-field");
       var columns = JSON.parse(container.getAttribute("data-columns"));
       var defaultRows = JSON.parse(container.getAttribute("data-default-rows") || "[]");
+      var existingRows = JSON.parse(container.getAttribute("data-existing-rows") || "[]");
       var rowIdx = 0;
 
-      if (defaultRows.length) {
+      if (existingRows.length) {
+        existingRows.forEach(function (row) {
+          addRow(container, field, columns, rowIdx++, row);
+        });
+      } else if (defaultRows.length) {
         defaultRows.forEach(function (label) {
           addRow(container, field, columns, rowIdx++, label);
         });
